fc4640f44ce372eafdb4a900a44d33f7ba5e357e
sunshicheng <sunshicheng@xiaozhu.com>
2019-09-30 14:43:51 +0800

    up

--- hello-rust/target/debug/deps/hello_rust-512c05c550627589.d
+++
@@ -1,5 +0,0 @@
-/home/ssc/code/rust_learn/hello-rust/target/debug/deps/hello_rust-512c05c550627589: src/main.rs
-
-/home/ssc/code/rust_learn/hello-rust/target/debug/deps/hello_rust-512c05c550627589.d: src/main.rs
-
-src/main.rs:
